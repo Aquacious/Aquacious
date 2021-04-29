b2f24d90a73f197f6a00f941295d2861509b8b38
llsc12 <42747613+llsc12@users.noreply.github.com>
2021-04-29 20:52:37 +0100

    add kick checkers

--- index.js
+++ index.js
@@ -289,9 +289,10 @@ client.on("message", async message => { //commands
 	}
 	if (command == 'kick') {
 		if (!message.member.hasPermission('KICK_MEMBERS', { checkAdmin: true, checkOwner: true })) return message.channel.send(deniedEmbed('You do not have Kick Members permission.')).then(x => {x.delete({timeout:5000})})
-		if (!args[0]) return message.channel.send(deniedEmbed('No user was specified.'))
-		if (!message.mentions.users.first()) return message.channel.send(deniedEmbed('Cannot find that user.'))
-		if (!message.guild.member(message.mentions.users.first())) return message.channel.send(deniedEmbed('Couldn\'t get member from user.'))
+		if (!args[0]) return message.channel.send(deniedEmbed('No user was specified.')).then(x => {x.delete({timeout:5000})})
+		if (!message.mentions.users.first()) return message.channel.send(deniedEmbed('Cannot find that user.')).then(x => {x.delete({timeout:5000})})
+		if (!message.guild.member(message.mentions.users.first())) return message.channel.send(deniedEmbed('Couldn\'t get member from user.')).then(x => {x.delete({timeout:5000})})
+		if (!message.guild.member(message.mentions.users.first()).kickable) return message.channel.send(deniedEmbed(`I'm unable to kick ${message.mentions.users.first().username}.`)).then(x => {x.delete({timeout:5000})})
 		let reason = 'No reason specified.'
 		if (args[1]) {
 			reason = args.join(' ').slice(args[0].length)
@@ -305,16 +306,17 @@ client.on("message", async message => { //commands
 		.setColor('RED')
 		.setThumbnail(message.mentions.users.first().avatarURL())
 		message.mentions.users.first().send(embed).catch(err => {message.channel.send('The user could not receive any details in DMs.')})
-		message.channel.send(embed)
+		message.channel.send(embed).then(x => {x.delete({timeout:15000})})
 		await sleep(500)
 		message.guild.member(message.mentions.users.first()).kick(reason)
 	}
 
 	if (command == 'ban') {
 		if (!message.member.hasPermission('BAN_MEMBERS', { checkAdmin: true, checkOwner: true })) return message.channel.send(deniedEmbed('You do not have Ban Members permission.')).then(x => {x.delete({timeout:5000})})
-		if (!args[0]) return message.channel.send(deniedEmbed('No user was specified.'))
-		if (!message.mentions.users.first()) return message.channel.send(deniedEmbed('Cannot find that user.'))
-		if (!message.guild.member(message.mentions.users.first())) return message.channel.send(deniedEmbed('Couldn\'t get member from user.'))
+		if (!args[0]) return message.channel.send(deniedEmbed('No user was specified.')).then(x => {x.delete({timeout:5000})})
+		if (!message.mentions.users.first()) return message.channel.send(deniedEmbed('Cannot find that user.')).then(x => {x.delete({timeout:5000})})
+		if (!message.guild.member(message.mentions.users.first())) return message.channel.send(deniedEmbed('Couldn\'t get member from user.')).then(x => {x.delete({timeout:5000})})
+		if (!message.guild.member(message.mentions.users.first()).bannable) return message.channel.send(`I'm unable to ban ${message.mentions.users.first().username}.`).then(x => {x.delete({timeout:5000})})
 		let reason = 'No reason specified.'
 		if (args[1]) {
 			reason = args.join(' ').slice(args[0].length)
@@ -327,8 +329,8 @@ client.on("message", async message => { //commands
 		.addField('Reason', reason)
 		.setColor('RED')
 		.setThumbnail(message.mentions.users.first().avatarURL())
-		message.mentions.users.first().send(embed).catch(err => {message.channel.send('The user could not receive any details about this incident in DMs.')})
-		message.channel.send(embed)
+		message.mentions.users.first().send(embed).catch(err => {message.channel.send('The user could not receive any details about this incident in DMs.')}).then(x => {x.delete({timeout:15000})})
+		message.channel.send(embed).then(x => {x.delete({timeout:15000})})
 		await sleep(500)
 		message.guild.member(message.mentions.users.first()).ban(reason)
 	}
