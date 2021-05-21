e20ebe24922d84c92f3b24213c7709d693e272eb
llsc12 <42747613+llsc12@users.noreply.github.com>
2021-05-21 10:09:53 +0100

    Create queueLoader.js

---
+++ commands/Music/queueLoader.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name:'queues',
+  category:'Music',
+  description:'Save and load queues online with others',
+  cooldown:15,
+  aliases:['qs'],
+  execute(client, message, args){ 
+    //code
+  }
+}
