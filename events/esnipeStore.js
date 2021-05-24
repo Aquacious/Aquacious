89fa6f216b348fb9588bf71799a687ba91bd874b
llsc12 <42747613+llsc12@users.noreply.github.com>
2021-05-24 18:23:24 +0100

    switching to yarn bc npm kinda sus

--- events/esnipeStore.js
+++ events/esnipeStore.js
@@ -3,6 +3,9 @@ module.exports = {
   name:"messageUpdate",
   execute(client, message) {
     if (message.author.bot || !message.guild) return
-    client.editedMessages.set(message.channel.id, message)
+    let array = client.editedMessages.get(message.channel.id)
+    if (!array[0]) array = new Array()
+    array.unshift(message)
+    client.editedMessages.set(message.channel.id, array)
   }
 }
--- events/snipeStore.js
+++ events/snipeStore.js
@@ -22,7 +22,9 @@ module.exports = {
 module.exports = {
   name:"messageDelete",
   execute(client, message) {
-    if (message.author.bot || !message.guild) return
-    client.deletedMessages.set(message.channel.id, message)
+    let array = client.deletedMessages.get(message.channel.id)
+    if (!array[0]) array = new Array()
+    array.unshift(message)
+    client.deletedMessages.set(message.channel.id, array)
   }
 }
