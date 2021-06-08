0c65e6ce5d1727af15ef6579e3bdefa4228a2811
llsc12 <42747613+llsc12@users.noreply.github.com>
2021-06-08 20:24:36 +0100

    internal sharding ig

--- client/Client.js
+++ client/Client.js
@@ -1,12 +1,12 @@
 const { Client, Collection } = require('discord.js');
 module.exports = class extends Client {
-	constructor(config) {
+	constructor() {
 		super({
 			disableMentions: 'everyone',
 			messageSweepInterval: 60,
+			shards: 'auto'
 		});
 		this.commands = new Collection();
 		this.queue = new Map();
-		this.config = config;
 	}
 };
