ae892afe899ab4fbd1319fbe46db2f4d774229a2
Intelli <pun6sn@gmail.com>
2021-08-15 11:23:10 +0300

    My Bad [Fixed Bad Commit]

--- commands/Fun/minesweep.js
+++ commands/Fun/minesweep.js
@@ -15,6 +15,9 @@ module.exports = {
       if (isNaN(gameWidth)) {
         gameWidth = 6;
       }
+      if (isNaN(gameHeight)) {
+        gameHeight = 6;
+      }
       else if (gameWidth <= 0 || gameHeight <= 0) {
         return `Uh, I'm not smart enough to generate a maze sized ${gameWidth} by ${gameHeight}. I can only use positive numbers. Sorry :cry:`;
       }
